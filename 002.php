3e44d77bea5be3f5f0c191c7d801297116208689
abigailao <aakile@roc-dev.com>
2021-04-13 19:23:54 +0200

    002

---
+++ 002.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>002</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<article class="all-002">
+    <h1>Beginnen met programmeren in PHP</h1>
+    <article class="bk-002">
+        <h2>Laat PHP onderstaande zin op het scherm zetten, maar maak zoveel mogelijk gebruik
+            van de variabelen $a1, $b1 en $c1.
+        </h2>
+        <p><?php
+            $a1 = "Ik heb niet gefaald";
+            $b1 = "die niet werken";
+            $c1 = 10000;
+
+            echo $a1.". Ik heb alleen ".$c1." manieren gevonden ".$b1.". <br><i>-Thomas Edison</i>";
+            ?></p>
+</article>
+<article class="bk-002">
+    <h2>Laat PHP onderstaande zin op het scherm zetten, maar maak zoveel mogelijk gebruik
+        van de variabelen $a2, $b2 en $c2.
+    </h2>
+    <p><?php
+        $a2 = "die geen fout";
+        $b2 = "en";
+        $c2 = "niet.";
+
+        echo "M".$b2."s".$b2." ".$a2.$b2." mak".$b2.", werk".$b2." ".$c2."<br>
+<a href='https://www.ikgastarten.nl/bedrijf-starten/de-start-van-klm'><i>-Albert Plesman (KLM)</i></a>";
+        ?></p>
+</article>
+<article class="bk-002">
+    <h2>Laat PHP onderstaande zin op het scherm zetten, maar maak zoveel mogelijk gebruik
+        van de variabelen $a3, $b3 en $c3.
+    </h2>
+    <p><?php
+        $a3 = "ry";
+        $b3 = ", but";
+        $c3 = "fail";
+
+        echo "T".$a3." and ".$c3.$b3." never ".$c3." to t".$a3.". <br><i> -Tyra Banks</i>";
+        ?></p>
+</article>
+<article class="bk-002">
+    <h2>Laat PHP onderstaande zin op het scherm zetten, maar maak zoveel mogelijk gebruik
+        van de variabelen $a4, $b4 en $c4.
+    </h2>
+    <p><?php
+        $a4 = "\"omgaan";
+        $b4 = "met";
+        $c4 = "teleu";
+
+        echo "De cursus ".$a4." ".$b4." ".$c4."rstellingen\" kan vanavond helaas niet doorgaan. <br>
+ <i>-Herman Finkers</i>";
+        ?></p>
+</article>
+</article>
+
+</body>
+</html>
---
+++ helloworld.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Hello World!";
+
+//link: https://www.savatarian.com/abigail/cohort9php/helloworld/index.php
